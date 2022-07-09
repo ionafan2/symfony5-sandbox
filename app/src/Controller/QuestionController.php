414c802eeaadd6504a1e0804fdcb9ad32cd52a32
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-07-09 14:53:21 -0700

    06. Flex, Recipes & Aliases 07. How Recipes Work 08. The Twig Recipe 09. Twig ❤️ 10. Profiler: Your Debugging Best Friend 11. Assets: CSS, Images, etc

--- app/src/Controller/QuestionController.php
+++ app/src/Controller/QuestionController.php
@@ -5,23 +5,37 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class QuestionController extends AbstractController
 {
-    #[Route('/', name: 'homepage')]
-    public function index(): Response
+    #[Route('/', name: 'app_homepage')]
+    public function homepage(Environment $twigEnvironment)
     {
-        return $this->render('question/index.html.twig', [
-            'controller_name' => 'QuestionController',
-        ]);
+        /*
+        // fun example of using the Twig service directly!
+        $html = $twigEnvironment->render('question/homepage.html.twig');
+
+        return new Response($html);
+        */
+
+        return $this->render('question/homepage.html.twig');
     }
+
 
     #[Route('/questions/{slug}', name: 'app_question')]
     public function show($slug): Response
     {
-        return $this->render('question/index.html.twig', [
+        $answers = [
+            'Make sure your cat is sitting purrrfectly still 🤣',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe... try saying the spell backwards?',
+        ];
+
+        return $this->render('question/show.html.twig', [
             'controller_name' => 'QuestionController',
-            'slug' => ucfirst(str_replace(search: "-", replace:" ", subject: $slug)),
+            'question' => ucfirst(str_replace(search: "-", replace:" ", subject: $slug)),
+            'answers' => $answers,
         ]);
     }
 
