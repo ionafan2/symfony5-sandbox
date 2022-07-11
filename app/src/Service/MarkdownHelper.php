7ea416b7efd8c99b9441a77af7064a3d20ac5327
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-07-11 16:08:48 -0700

    15. Named Autowiring

--- app/src/Service/MarkdownHelper.php
+++ app/src/Service/MarkdownHelper.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 namespace App\Service;
 
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 
 class MarkdownHelper
@@ -20,13 +21,16 @@ class MarkdownHelper
     public function __construct(
         private MarkdownParserInterface $markdownParser,
         private CacheInterface          $cache,
-        private bool $isDebug
+        private bool $isDebug,
+        private LoggerInterface $markdownLogger
     )
     {
     }
 
     public function parse(string $string): string
     {
+        $this->markdownLogger->info("");
+
         if (!$this->isDebug) {
             return $this->markdownParser->transformMarkdown($string);
         }
