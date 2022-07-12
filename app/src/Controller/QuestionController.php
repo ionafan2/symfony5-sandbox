93bc45f56a9703c843079c367b239986f1af6baa
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-07-12 16:31:17 -0700

    17. Request Object & POST Data

--- app/src/Controller/QuestionController.php
+++ app/src/Controller/QuestionController.php
@@ -7,6 +7,7 @@ use App\Repository\QuestionRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -80,6 +81,26 @@ EOF
         return $this->render('question/show.html.twig', [
             'question' => $question,
             'answers' => $answers
+        ]);
+    }
+
+    /**
+     * @Route("/questions/{slug}/vote", name="app_question_vote", methods="POST")
+     */
+    public function questionVote(Question $question, Request $request, EntityManagerInterface $entityManager)
+    {
+        $direction = $request->request->get('direction');
+
+        if ($direction === 'up') {
+            $question->upVote();
+        } elseif ($direction === 'down') {
+            $question->downVote();
+        }
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_question_show', [
+            'slug' => $question->getSlug()
         ]);
     }
 }
