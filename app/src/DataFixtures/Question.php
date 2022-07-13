75c27a768dbb96c9edae672967e12311c8732d8d
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-07-12 18:21:21 -0700

    20. Foundry: Fixture Model Factories

--- app/src/DataFixtures/Question.php
+++ app/src/DataFixtures/Question.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\QuestionFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,23 +10,6 @@ class Question extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $question = new \App\Entity\Question();
-        $question->setName('Missing pants')
-            ->setSlug('missing-pants-' . rand(0, 1000))
-            ->setVotes(rand(-10, 10))
-            ->setQuestion(question: <<<EOF
-Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-Accusantium aliquam, at consectetur cupiditate ea exercitationem facilis iusto laudantium maxime minima nisi placeat quasi,
- quibusdam repudiandae tempora tenetur veniam vero. Delectus.
-EOF
-            );
-
-        if (rand(1, 10) > 2) {
-            $question->setAskedAt(new \DateTimeImmutable(sprintf('-%d days', rand(1, 10))));
-        }
-
-        $manager->persist($question);
-
-        $manager->flush();
+        QuestionFactory::createMany(5);
     }
 }
