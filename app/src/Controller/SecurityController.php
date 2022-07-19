09f9858194ec9ba7bdeab52037145cc961b76a6a
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-07-19 14:01:50 -0700

    09. When Authentication Fails

--- app/src/Controller/SecurityController.php
+++ app/src/Controller/SecurityController.php
@@ -5,12 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route('/auth/login', name: 'app_auth_login')]
-    public function index(): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('/auth/login.html.twig' );
+        return $this->render('/auth/login.html.twig', [
+            'error' => $authenticationUtils->getLastAuthenticationError()
+        ]);
     }
 }
