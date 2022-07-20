30ab05b20edeb56eba829190274c05bd38b84c6a
Pavlo Cherniavskyi <admin@staysmartway.com>
2022-07-19 19:48:04 -0700

    28. User API & the Serializer

--- app/src/Entity/User.php
+++ app/src/Entity/User.php
@@ -6,6 +6,7 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
@@ -13,15 +14,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
+    #[Groups('user:read')]
     private $id;
 
     #[ORM\Column(type: 'string', length: 180, unique: true)]
+    #[Groups('user:read')]
     private ?string $email;
 
     #[ORM\Column(type: 'json')]
     private array $roles = [];
 
     #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    #[Groups('user:read')]
     private ?string $firstName;
 
     #[ORM\Column(type: 'string', length: 255)]
@@ -142,6 +146,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
+    #[Groups('user:read')]
     public function getAvatarUri(int $size = 35): string
     {
         return "https://ui-avatars.com/api/?" . http_build_query([
